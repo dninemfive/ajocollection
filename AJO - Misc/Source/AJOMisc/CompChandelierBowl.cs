9565b423c354f6bb28b4758c0462cabf87ea218a
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-04-20 14:51:17 -0700

    work on chandelier bowl

---
+++ AJO - Misc/Source/AJOMisc/CompChandelierBowl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AJOMisc
+{
+    class CompChandelierBowl : ThingComp
+    {
+        public override void PostDraw()
+        {
+
+        }
+    }
+}
