dc2087a89408282eba9988e8d8ff1365c08af80b
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-10-17 16:02:19 -0700

    add work defs

--- AJO - Bonsai/Source/AJOBonsai/AJOBonsaiDefOf.cs
+++ AJO - Bonsai/Source/AJOBonsai/AJOBonsaiDefOf.cs
@@ -11,7 +11,7 @@ namespace AJOBonsai
     [DefOf]
     public static class AJOBonsaiDefOf
     {
-        public static JobDef AJO_BonsaiMaintain;
+        public static JobDef D9AJO_J_MaintainBonsai;
 
         static AJOBonsaiDefOf()
         {
